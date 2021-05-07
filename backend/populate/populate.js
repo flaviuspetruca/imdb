c3a8e71e1f13ede25612410920a336b14ed42a15
Bogdan Silvasan <bogdan.silve@gmail.com>
2021-05-07 18:32:34 +0300

    Populate books and modified book schema

--- backend/populate/populate.js
+++ backend/populate/populate.js
@@ -2,15 +2,23 @@ const Books = require('../schemas/book');
 
 const populate = (req, res) => {
     let body = req.body;
-    console.log(body);
-    for (const item of body) {
-        let book = new Books(body);
-        book.save().then().catch((error) => {
-            console.log(error);
+    for (item of body) {
+        let book = new Books({
+            title: item.volumeInfo.title,
+            subtitle: item.volumeInfo.subtitle,
+            authors: item.volumeInfo.authors,
+            publisher: item.volumeInfo.publisher,
+            publishedDate: item.volumeInfo.publishedDate,
+            categories: item.volumeInfo.categories,
+            ...(item.volumeInfo.imageLinks && { thumbnail: item.volumeInfo.imageLinks.thumbnail }),
+            description: item.volumeInfo.description,
+            purchaseLink: item.volumeInfo.infoLink
         });
-    }
 
-    
+        console.log(book);
+
+        book.save();
+    }
 }
 
 module.exports = populate;
